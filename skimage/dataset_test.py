f091b7eb0160b80f4c6f0d3af7cda727b54c2f30
MURAKI Taichi <muraki.taichi@gmail.com>
2022-09-21 13:13:53 +0900

    fix

--- skimage/dataset_test.py
+++ skimage/dataset_test.py
@@ -2,17 +2,20 @@ from pathlib import Path
 from dataset import RingFingerDataset
 
 #base_data_dir = Path("data/outputs")
-base_data_dir = Path("./data/outputs/")
+base_data_dir = Path("../../ring-finger-semseg/data/outputs/")
 
-numbers_json_filepath = "data/contour_checked_numbers.json"
+numbers_json = {
+    "training": "data/contour_checked_numbers_training.json",
+    "validation": "data/contour_checked_numbers_validation.json",
+}
 
-train_dataset = RingFingerDataset(base_data_dir / "training", numbers_json_filepath)
-valid_dataset = RingFingerDataset(base_data_dir / "validation", numbers_json_filepath)
+train_dataset = RingFingerDataset(base_data_dir / "training", numbers_json["training"])
+valid_dataset = RingFingerDataset(base_data_dir / "validation", numbers_json["validation"])
 
 from torch import nn
 from torch.utils.data import DataLoader
 
-train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=8)
 valid_dataloader = DataLoader(valid_dataset, batch_size=8)
 
 from tqdm import tqdm
